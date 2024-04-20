bce76057e2403f877f75b70a84036802a965b725
DevosKaizen <andrew.andyoo@gmail.com>
2024-04-20 13:45:06 -0300

    27% do curso

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: "Aprendendo Angular",
+    autoria: "Devos",
+    modelo: ''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
