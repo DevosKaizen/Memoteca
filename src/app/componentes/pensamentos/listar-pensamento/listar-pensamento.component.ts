37830443d2c59c54186e0948dd2445c47888e631
DevosKaizen <andrew.andyoo@gmail.com>
2024-04-21 15:28:42 -0300

    Tipos diferentes de diretivas; Utilizar diretivas estruturais incluindo e excluindo elementos com ngFor e ngIf; Utilizar expressões para estilizar elementos com ngClass.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -14,10 +14,15 @@ export class ListarPensamentoComponent implements OnInit {
       modelo:'modelo3',
     },
     {
+      conteudo: '"Na vastidão do universo, nossa existência é um breve lampejo de consciência. Em meio ao mistério e à incerteza, buscamos significado. Somos estrelas cadentes na noite cósmica, deixando rastros de questionamentos na eternidade.Na vastidão do universo, nossa existência é um breve lampejo de consciência. Em meio ao mistério e à incerteza, buscamos significado. Somos estrelas cadentes na noite cósmica, deixando rastros de questionamentos na eternidade"',
+      autoria:'IA GPT',
+      modelo:'modelo3',
+    },
+    {
       conteudo: 'minha propriedade é decorada com @Input()',
       autoria:'Componente filho',
       modelo:'modelo3',
-    }
+    },
   ];
 
   constructor() { }
