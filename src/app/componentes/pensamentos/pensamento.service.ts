9905e89f6c3bece06bcde783f520f7bd7980479d
AndrewDevos <48452066+DevosKaizen@users.noreply.github.com>
2024-04-24 18:28:27 -0300

    ngOnInit(): void {     this.service.listar()

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,10 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pensamento } from './pensamentos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
+    private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) { }
+
+  listar(){
+    return this.http.get<Pensamento[]>(this.API);
+  }
 }
